fbfb6fff46ec80c6bc2314328933a8bd901805d6
SKIPPYNUTSHQ\Aaron <Aaron.Kendall@gmail.com>
2025-02-01 19:31:59 -0500

    Initial revision

---
+++ NethereumSmartObjectFramework/Nethereum.Erc20.Blazor/EveFrontierModels/EveFrontierStorageTradeModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Numerics;
+
+using Nethereum.UI;
+
+using CCP.EveFrontier.SOF.SmartStorageUnitJaerith.Systems.SmartStorageUnitSystem.ContractDefinition;
+
+namespace Nethereum.Erc20.Blazor.EveFrontierModels
+{
+    public class EveFrontierStorageTradeModel
+    {
+        public static readonly BigInteger DEFAULT_STORAGE_SMART_OBJECT_ID =
+            BigInteger.Parse("17614304337475056394242299294383532840873792487945557467064313427436901763824");
+
+        public static readonly BigInteger DEFAULT_ITEM_IN_SMART_OBJECT_ID =
+            BigInteger.Parse("72303041834441799565597028082148290553073890313361053989246429514519533100781");
+
+        public EveFrontierWorldModel EveFrontierWorld { get; set; } = new EveFrontierWorldModel();
+
+        public BigInteger StorageSmartObjectId { get; set; } = DEFAULT_STORAGE_SMART_OBJECT_ID;
+
+        public BigInteger ItemInSmartObjectId { get; set; } = DEFAULT_ITEM_IN_SMART_OBJECT_ID;
+
+        public ulong ItemInQuantity { get; set; }
+
+        public ExecuteFunction GetExecuteFunction()
+        {
+            return new ExecuteFunction()
+            {
+                SmartObjectId = StorageSmartObjectId,
+                InventoryItemIdIn = ItemInSmartObjectId,
+                Quantity = ItemInQuantity
+            };
+        }
+    }
+}
---
+++ NethereumSmartObjectFramework/Nethereum.Erc20.Blazor/EveFrontierModels/EveFrontierWorldModel.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Nethereum.Erc20.Blazor.EveFrontierModels
+{
+    public class EveFrontierWorldModel
+    {
+        public const string DEFAULT_WORLD_CONTRACT_ADDRESS = "0x8a791620dd6260079bf849dc5567adc3f2fdc318";
+
+        public const string DEFAULT_WORLD_URL = "http://localhost:8545";
+
+        public EveFrontierWorldModel() { }
+
+        public string Address { get; set; } = DEFAULT_WORLD_CONTRACT_ADDRESS;
+
+        public string Url { get; set; } = DEFAULT_WORLD_URL;
+    }
+}
+
