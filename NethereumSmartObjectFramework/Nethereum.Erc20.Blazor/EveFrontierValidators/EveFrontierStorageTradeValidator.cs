34885579809a35dcf1fbccde83a6f227e7a3269d
SKIPPYNUTSHQ\Aaron <Aaron.Kendall@gmail.com>
2025-02-02 14:44:12 -0500

    Initial revision

---
+++ NethereumSmartObjectFramework/Nethereum.Erc20.Blazor/EveFrontierValidators/EveFrontierStorageTradeValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nethereum.UI.Validation;
+using Microsoft.AspNetCore.Components;
+
+using Nethereum.Erc20.Blazor.EveFrontierModels;
+
+namespace Nethereum.Erc20.Blazor.EveFrontierValidators
+{
+    public class EveFrontierStorageTradeValidator : AbstractValidator<EveFrontierStorageTradeModel>
+    {
+        public EveFrontierStorageTradeValidator()
+        {
+            RuleFor(t => (long) t.ItemInQuantity).GreaterThan(0).WithMessage("Amount must be greater than 0");
+
+            RuleFor(t => t.EveFrontierWorld).SetValidator(new EveFrontierWorldValidator());
+        }
+    }
+}
---
+++ NethereumSmartObjectFramework/Nethereum.Erc20.Blazor/EveFrontierValidators/EveFrontierWorldValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Nethereum.UI.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Nethereum.Erc20.Blazor.EveFrontierModels;
+
+namespace Nethereum.Erc20.Blazor.EveFrontierValidators
+{
+    public class EveFrontierWorldValidator : AbstractValidator<EveFrontierWorldModel>
+    {
+        public EveFrontierWorldValidator()
+        {
+            RuleFor(t => t.Address).IsEthereumAddress();
+            RuleFor(t => t.Url).IsUri();
+        }
+    }
+}
