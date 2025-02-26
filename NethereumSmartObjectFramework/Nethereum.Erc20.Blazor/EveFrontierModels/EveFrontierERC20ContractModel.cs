200ffc200a70e8d512201c5ec2d6b7eed3f603fd
SKIPPYNUTSHQ\Aaron <Aaron.Kendall@gmail.com>
2025-02-25 23:12:56 -0500

    Initial revision

---
+++ NethereumSmartObjectFramework/Nethereum.Erc20.Blazor/EveFrontierModels/EveFrontierERC20ContractModel.cs
@@ -0,0 +1,13 @@
+﻿using Nethereum.Erc20.Blazor.EveFrontier;
+
+namespace Nethereum.Erc20.Blazor.EveFrontierModels
+{
+    public class EveFrontierERC20ContractModel
+    {
+        public EveFrontierWorldModel EveFrontierWorld { get; set; } = new EveFrontierWorldModel();
+
+        public EveFrontierERC20ContractModel() { }
+
+        public string ERC20Namespace { get; set; }
+    }
+}
