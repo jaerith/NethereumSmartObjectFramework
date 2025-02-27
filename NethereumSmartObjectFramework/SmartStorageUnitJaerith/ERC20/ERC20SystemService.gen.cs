63326cdb06ee46a66ffe1f89271a45c6a0a7a86c
SKIPPYNUTSHQ\Aaron <Aaron.Kendall@gmail.com>
2025-02-26 23:43:36 -0500

    Initial revision.

---
+++ NethereumSmartObjectFramework/SmartStorageUnitJaerith/ERC20/ERC20SystemService.gen.cs
@@ -0,0 +1,359 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Numerics;
+using Nethereum.Hex.HexTypes;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Web3;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Contracts.CQS;
+using Nethereum.Contracts.ContractHandlers;
+using Nethereum.Contracts;
+using System.Threading;
+using CCP.EveFrontier.SOF.SmartStorageUnitJaerith.ERC20.ERC20System.ContractDefinition;
+
+namespace CCP.EveFrontier.SOF.SmartStorageUnitJaerith.ERC20.ERC20System
+{
+    public partial class ERC20SystemService: ERC20SystemServiceBase
+    {
+        public static Task<TransactionReceipt> DeployContractAndWaitForReceiptAsync(Nethereum.Web3.IWeb3 web3, ERC20SystemDeployment eRC20SystemDeployment, CancellationTokenSource cancellationTokenSource = null)
+        {
+            return web3.Eth.GetContractDeploymentHandler<ERC20SystemDeployment>().SendRequestAndWaitForReceiptAsync(eRC20SystemDeployment, cancellationTokenSource);
+        }
+
+        public static Task<string> DeployContractAsync(Nethereum.Web3.IWeb3 web3, ERC20SystemDeployment eRC20SystemDeployment)
+        {
+            return web3.Eth.GetContractDeploymentHandler<ERC20SystemDeployment>().SendRequestAsync(eRC20SystemDeployment);
+        }
+
+        public static async Task<ERC20SystemService> DeployContractAndGetServiceAsync(Nethereum.Web3.IWeb3 web3, ERC20SystemDeployment eRC20SystemDeployment, CancellationTokenSource cancellationTokenSource = null)
+        {
+            var receipt = await DeployContractAndWaitForReceiptAsync(web3, eRC20SystemDeployment, cancellationTokenSource);
+            return new ERC20SystemService(web3, receipt.ContractAddress);
+        }
+
+        public ERC20SystemService(Nethereum.Web3.IWeb3 web3, string contractAddress) : base(web3, contractAddress)
+        {
+        }
+
+    }
+
+
+    public partial class ERC20SystemServiceBase: ContractWeb3ServiceBase
+    {
+
+        public ERC20SystemServiceBase(Nethereum.Web3.IWeb3 web3, string contractAddress) : base(web3, contractAddress)
+        {
+        }
+
+        public Task<string> MsgSenderQueryAsync(MsgSenderFunction msgSenderFunction, BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<MsgSenderFunction, string>(msgSenderFunction, blockParameter);
+        }
+
+        
+        public virtual Task<string> MsgSenderQueryAsync(BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<MsgSenderFunction, string>(null, blockParameter);
+        }
+
+        public Task<BigInteger> MsgValueQueryAsync(MsgValueFunction msgValueFunction, BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<MsgValueFunction, BigInteger>(msgValueFunction, blockParameter);
+        }
+
+        
+        public virtual Task<BigInteger> MsgValueQueryAsync(BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<MsgValueFunction, BigInteger>(null, blockParameter);
+        }
+
+        public Task<string> WorldQueryAsync(WorldFunction worldFunction, BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<WorldFunction, string>(worldFunction, blockParameter);
+        }
+
+        
+        public virtual Task<string> WorldQueryAsync(BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<WorldFunction, string>(null, blockParameter);
+        }
+
+        public Task<BigInteger> AllowanceQueryAsync(AllowanceFunction allowanceFunction, BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<AllowanceFunction, BigInteger>(allowanceFunction, blockParameter);
+        }
+
+        
+        public virtual Task<BigInteger> AllowanceQueryAsync(string owner, string spender, BlockParameter blockParameter = null)
+        {
+            var allowanceFunction = new AllowanceFunction();
+                allowanceFunction.Owner = owner;
+                allowanceFunction.Spender = spender;
+            
+            return ContractHandler.QueryAsync<AllowanceFunction, BigInteger>(allowanceFunction, blockParameter);
+        }
+
+        public virtual Task<string> ApproveRequestAsync(ApproveFunction approveFunction)
+        {
+             return ContractHandler.SendRequestAsync(approveFunction);
+        }
+
+        public virtual Task<TransactionReceipt> ApproveRequestAndWaitForReceiptAsync(ApproveFunction approveFunction, CancellationTokenSource cancellationToken = null)
+        {
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(approveFunction, cancellationToken);
+        }
+
+        public virtual Task<string> ApproveRequestAsync(string spender, BigInteger value)
+        {
+            var approveFunction = new ApproveFunction();
+                approveFunction.Spender = spender;
+                approveFunction.Value = value;
+            
+             return ContractHandler.SendRequestAsync(approveFunction);
+        }
+
+        public virtual Task<TransactionReceipt> ApproveRequestAndWaitForReceiptAsync(string spender, BigInteger value, CancellationTokenSource cancellationToken = null)
+        {
+            var approveFunction = new ApproveFunction();
+                approveFunction.Spender = spender;
+                approveFunction.Value = value;
+            
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(approveFunction, cancellationToken);
+        }
+
+        public Task<BigInteger> BalanceOfQueryAsync(BalanceOfFunction balanceOfFunction, BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<BalanceOfFunction, BigInteger>(balanceOfFunction, blockParameter);
+        }
+
+        
+        public virtual Task<BigInteger> BalanceOfQueryAsync(string account, BlockParameter blockParameter = null)
+        {
+            var balanceOfFunction = new BalanceOfFunction();
+                balanceOfFunction.Account = account;
+            
+            return ContractHandler.QueryAsync<BalanceOfFunction, BigInteger>(balanceOfFunction, blockParameter);
+        }
+
+        public virtual Task<string> BurnRequestAsync(BurnFunction burnFunction)
+        {
+             return ContractHandler.SendRequestAsync(burnFunction);
+        }
+
+        public virtual Task<TransactionReceipt> BurnRequestAndWaitForReceiptAsync(BurnFunction burnFunction, CancellationTokenSource cancellationToken = null)
+        {
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(burnFunction, cancellationToken);
+        }
+
+        public virtual Task<string> BurnRequestAsync(string account, BigInteger value)
+        {
+            var burnFunction = new BurnFunction();
+                burnFunction.Account = account;
+                burnFunction.Value = value;
+            
+             return ContractHandler.SendRequestAsync(burnFunction);
+        }
+
+        public virtual Task<TransactionReceipt> BurnRequestAndWaitForReceiptAsync(string account, BigInteger value, CancellationTokenSource cancellationToken = null)
+        {
+            var burnFunction = new BurnFunction();
+                burnFunction.Account = account;
+                burnFunction.Value = value;
+            
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(burnFunction, cancellationToken);
+        }
+
+        public Task<byte> DecimalsQueryAsync(DecimalsFunction decimalsFunction, BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<DecimalsFunction, byte>(decimalsFunction, blockParameter);
+        }
+
+        
+        public virtual Task<byte> DecimalsQueryAsync(BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<DecimalsFunction, byte>(null, blockParameter);
+        }
+
+        public virtual Task<string> MintRequestAsync(MintFunction mintFunction)
+        {
+             return ContractHandler.SendRequestAsync(mintFunction);
+        }
+
+        public virtual Task<TransactionReceipt> MintRequestAndWaitForReceiptAsync(MintFunction mintFunction, CancellationTokenSource cancellationToken = null)
+        {
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(mintFunction, cancellationToken);
+        }
+
+        public virtual Task<string> MintRequestAsync(string account, BigInteger value)
+        {
+            var mintFunction = new MintFunction();
+                mintFunction.Account = account;
+                mintFunction.Value = value;
+            
+             return ContractHandler.SendRequestAsync(mintFunction);
+        }
+
+        public virtual Task<TransactionReceipt> MintRequestAndWaitForReceiptAsync(string account, BigInteger value, CancellationTokenSource cancellationToken = null)
+        {
+            var mintFunction = new MintFunction();
+                mintFunction.Account = account;
+                mintFunction.Value = value;
+            
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(mintFunction, cancellationToken);
+        }
+
+        public Task<string> NameQueryAsync(NameFunction nameFunction, BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<NameFunction, string>(nameFunction, blockParameter);
+        }
+
+        
+        public virtual Task<string> NameQueryAsync(BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<NameFunction, string>(null, blockParameter);
+        }
+
+        public Task<bool> SupportsInterfaceQueryAsync(SupportsInterfaceFunction supportsInterfaceFunction, BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<SupportsInterfaceFunction, bool>(supportsInterfaceFunction, blockParameter);
+        }
+
+        
+        public virtual Task<bool> SupportsInterfaceQueryAsync(byte[] interfaceId, BlockParameter blockParameter = null)
+        {
+            var supportsInterfaceFunction = new SupportsInterfaceFunction();
+                supportsInterfaceFunction.InterfaceId = interfaceId;
+            
+            return ContractHandler.QueryAsync<SupportsInterfaceFunction, bool>(supportsInterfaceFunction, blockParameter);
+        }
+
+        public Task<string> SymbolQueryAsync(SymbolFunction symbolFunction, BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<SymbolFunction, string>(symbolFunction, blockParameter);
+        }
+
+        
+        public virtual Task<string> SymbolQueryAsync(BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<SymbolFunction, string>(null, blockParameter);
+        }
+
+        public Task<BigInteger> TotalSupplyQueryAsync(TotalSupplyFunction totalSupplyFunction, BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<TotalSupplyFunction, BigInteger>(totalSupplyFunction, blockParameter);
+        }
+
+        
+        public virtual Task<BigInteger> TotalSupplyQueryAsync(BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<TotalSupplyFunction, BigInteger>(null, blockParameter);
+        }
+
+        public virtual Task<string> TransferRequestAsync(TransferFunction transferFunction)
+        {
+             return ContractHandler.SendRequestAsync(transferFunction);
+        }
+
+        public virtual Task<TransactionReceipt> TransferRequestAndWaitForReceiptAsync(TransferFunction transferFunction, CancellationTokenSource cancellationToken = null)
+        {
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(transferFunction, cancellationToken);
+        }
+
+        public virtual Task<string> TransferRequestAsync(string to, BigInteger value)
+        {
+            var transferFunction = new TransferFunction();
+                transferFunction.To = to;
+                transferFunction.Value = value;
+            
+             return ContractHandler.SendRequestAsync(transferFunction);
+        }
+
+        public virtual Task<TransactionReceipt> TransferRequestAndWaitForReceiptAsync(string to, BigInteger value, CancellationTokenSource cancellationToken = null)
+        {
+            var transferFunction = new TransferFunction();
+                transferFunction.To = to;
+                transferFunction.Value = value;
+            
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(transferFunction, cancellationToken);
+        }
+
+        public virtual Task<string> TransferFromRequestAsync(TransferFromFunction transferFromFunction)
+        {
+             return ContractHandler.SendRequestAsync(transferFromFunction);
+        }
+
+        public virtual Task<TransactionReceipt> TransferFromRequestAndWaitForReceiptAsync(TransferFromFunction transferFromFunction, CancellationTokenSource cancellationToken = null)
+        {
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(transferFromFunction, cancellationToken);
+        }
+
+        public virtual Task<string> TransferFromRequestAsync(string from, string to, BigInteger value)
+        {
+            var transferFromFunction = new TransferFromFunction();
+                transferFromFunction.From = from;
+                transferFromFunction.To = to;
+                transferFromFunction.Value = value;
+            
+             return ContractHandler.SendRequestAsync(transferFromFunction);
+        }
+
+        public virtual Task<TransactionReceipt> TransferFromRequestAndWaitForReceiptAsync(string from, string to, BigInteger value, CancellationTokenSource cancellationToken = null)
+        {
+            var transferFromFunction = new TransferFromFunction();
+                transferFromFunction.From = from;
+                transferFromFunction.To = to;
+                transferFromFunction.Value = value;
+            
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(transferFromFunction, cancellationToken);
+        }
+
+        public override List<Type> GetAllFunctionTypes()
+        {
+            return new List<Type>
+            {
+                typeof(MsgSenderFunction),
+                typeof(MsgValueFunction),
+                typeof(WorldFunction),
+                typeof(AllowanceFunction),
+                typeof(ApproveFunction),
+                typeof(BalanceOfFunction),
+                typeof(BurnFunction),
+                typeof(DecimalsFunction),
+                typeof(MintFunction),
+                typeof(NameFunction),
+                typeof(SupportsInterfaceFunction),
+                typeof(SymbolFunction),
+                typeof(TotalSupplyFunction),
+                typeof(TransferFunction),
+                typeof(TransferFromFunction)
+            };
+        }
+
+        public override List<Type> GetAllEventTypes()
+        {
+            return new List<Type>
+            {
+                typeof(ApprovalEventDTO),
+                typeof(StoreSplicestaticdataEventDTO),
+                typeof(TransferEventDTO)
+            };
+        }
+
+        public override List<Type> GetAllErrorTypes()
+        {
+            return new List<Type>
+            {
+                typeof(ERC20InsufficientAllowanceError),
+                typeof(ERC20InsufficientBalanceError),
+                typeof(ERC20InvalidApproverError),
+                typeof(ERC20InvalidReceiverError),
+                typeof(ERC20InvalidSenderError),
+                typeof(ERC20InvalidSpenderError),
+                typeof(PuppetmasterNopuppetError),
+                typeof(SliceOutofboundsError),
+                typeof(WorldAccessdeniedError)
+            };
+        }
+    }
+}
---
+++ NethereumSmartObjectFramework/SmartStorageUnitJaerith/ERC20/ERC20SystemServiceMudExt.gen.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Numerics;
+using Nethereum.Mud.Contracts.Core.Systems;
+using Nethereum.Hex.HexTypes;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.ABI.Model;
+using Nethereum.ABI.FunctionEncoding;
+using Nethereum.Contracts.Create2Deployment;
+using Nethereum.Mud;
+using Nethereum.Web3;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Contracts.CQS;
+using Nethereum.Contracts.ContractHandlers;
+using Nethereum.Contracts;
+using System.Threading;
+using CCP.EveFrontier.SOF.SmartStorageUnitJaerith.ERC20.ERC20System.ContractDefinition;
+
+namespace CCP.EveFrontier.SOF.SmartStorageUnitJaerith.ERC20.ERC20System
+{
+    public class ERC20SystemServiceResource : SystemResource
+    {
+        public ERC20SystemServiceResource() : base("ERC20System", "GoblinKingCorp") { }
+    }
+    public partial class ERC20SystemService: ISystemService<ERC20SystemServiceResource> 
+    {
+        public IResource Resource => this.GetResource();
+
+        public ISystemServiceResourceRegistration SystemServiceResourceRegistrator
+        {
+            get
+            {
+                return this.GetSystemServiceResourceRegistration<ERC20SystemServiceResource, ERC20SystemService>();
+            }
+        }
+        
+
+        public virtual List<FunctionABI> GetSystemFunctionABIs()
+        {
+            return GetAllFunctionABIs();
+        }
+
+        public virtual string CalculateCreate2Address(string deployerAddress, string salt, params ByteCodeLibrary[] byteCodeLibraries)
+        {
+            return new ERC20SystemDeployment().CalculateCreate2Address(deployerAddress, salt, byteCodeLibraries);
+        }
+
+        public virtual Task<Create2ContractDeploymentTransactionResult> DeployCreate2ContractAsync(string deployerAddress, string salt, params ByteCodeLibrary[] byteCodeLibraries)
+        {
+            var create2ProxyDeployerService = Web3.Eth.Create2DeterministicDeploymentProxyService;
+            var deployment = new ERC20SystemDeployment();
+            return create2ProxyDeployerService.DeployContractRequestAsync(deployment, deployerAddress, salt, byteCodeLibraries);
+        }
+        public virtual Task<Create2ContractDeploymentTransactionReceiptResult> DeployCreate2ContractAndWaitForReceiptAsync(string deployerAddress, string salt, ByteCodeLibrary[] byteCodeLibraries, CancellationToken cancellationToken = default)
+        {
+            var create2ProxyDeployerService = Web3.Eth.Create2DeterministicDeploymentProxyService;
+            var deployment = new ERC20SystemDeployment();
+            return create2ProxyDeployerService.DeployContractRequestAndWaitForReceiptAsync(deployment, deployerAddress, salt, byteCodeLibraries, cancellationToken);
+        }
+    }
+}
