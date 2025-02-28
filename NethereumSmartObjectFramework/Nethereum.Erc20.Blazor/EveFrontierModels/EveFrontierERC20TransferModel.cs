50315fad95129a41ce61479789202bfffc0fbfc3
SKIPPYNUTSHQ\Aaron <Aaron.Kendall@gmail.com>
2025-02-27 23:35:45 -0500

    Initial revision.

---
+++ NethereumSmartObjectFramework/Nethereum.Erc20.Blazor/EveFrontierModels/EveFrontierERC20TransferModel.cs
@@ -0,0 +1,26 @@
+﻿using CCP.EveFrontier.SOF.SmartStorageUnitJaerith.ERC20.ERC20System.ContractDefinition;
+
+namespace Nethereum.Erc20.Blazor.EveFrontierModels
+{
+    public class EveFrontierERC20TransferModel
+    {
+        public EveFrontierERC20ContractModel ERC20Contract { get; set; } = new EveFrontierERC20ContractModel();
+
+        public EveFrontierERC20TransferModel() { }
+
+        public string To { get; set; }
+
+        public decimal Value { get; set; }
+
+        public TransferFunction GetTransferFunction()
+        {
+            return new TransferFunction()
+            {
+                To = To,
+                Value = Web3.Web3.Convert.ToWei(Value, ERC20Contract.DecimalPlaces),
+                AmountToSend = 0
+            };
+        }
+
+    }
+}
