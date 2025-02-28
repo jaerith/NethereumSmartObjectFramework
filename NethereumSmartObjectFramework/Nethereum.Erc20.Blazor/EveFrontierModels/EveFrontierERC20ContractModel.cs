9520222fbe8a26dcb982709ae891b4a5882468b7
SKIPPYNUTSHQ\Aaron <Aaron.Kendall@gmail.com>
2025-02-27 23:35:28 -0500

    Added decimal place property for amount conversions.

--- NethereumSmartObjectFramework/Nethereum.Erc20.Blazor/EveFrontierModels/EveFrontierERC20ContractModel.cs
+++ NethereumSmartObjectFramework/Nethereum.Erc20.Blazor/EveFrontierModels/EveFrontierERC20ContractModel.cs
@@ -4,6 +4,10 @@ namespace Nethereum.Erc20.Blazor.EveFrontierModels
 {
     public class EveFrontierERC20ContractModel
     {
+        public const int DEFAULT_DECIMALS = 18;
+
+        public int DecimalPlaces { get; set; } = DEFAULT_DECIMALS;
+
         public EveFrontierWorldModel EveFrontierWorld { get; set; } = new EveFrontierWorldModel();
 
         public EveFrontierERC20ContractModel() { }
