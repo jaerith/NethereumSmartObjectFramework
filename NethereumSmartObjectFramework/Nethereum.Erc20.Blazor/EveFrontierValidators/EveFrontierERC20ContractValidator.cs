f983818536fd19df6477a0c8f002d6e94f4f6075
SKIPPYNUTSHQ\Aaron <Aaron.Kendall@gmail.com>
2025-02-28 22:45:09 -0500

    Initial revision

---
+++ NethereumSmartObjectFramework/Nethereum.Erc20.Blazor/EveFrontierValidators/EveFrontierERC20ContractValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Nethereum.Erc20.Blazor.EveFrontierModels;
+using Nethereum.UI.Validation;
+
+namespace Nethereum.Erc20.Blazor
+{
+    public class EveFrontierERC20ContractValidator : AbstractValidator<EveFrontierERC20ContractModel>
+    {
+        public EveFrontierERC20ContractValidator()
+        {
+            RuleFor(t => t.EveFrontierWorld.Address).IsEthereumAddress();
+            RuleFor(t => t.DecimalPlaces).GreaterThan(0).WithMessage("Decimal Places must be greater than 0");
+        }
+    }
+}
---
+++ NethereumSmartObjectFramework/Nethereum.Erc20.Blazor/EveFrontierValidators/EveFrontierERC20TransferValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Nethereum.Erc20.Blazor.EveFrontierModels;
+using Nethereum.UI.Validation;
+
+namespace Nethereum.Erc20.Blazor
+{
+    public class EveFrontierERC20TransferValidator : AbstractValidator<EveFrontierERC20TransferModel>
+    {
+        public EveFrontierERC20TransferValidator()
+        {
+            RuleFor(t => t.To).IsEthereumAddress();
+            RuleFor(t => t.Value).GreaterThan(0).WithMessage("Amount must be greater than 0");
+            RuleFor(t => t.ERC20Contract).SetValidator(new EveFrontierERC20ContractValidator());
+        }
+    }
+}
