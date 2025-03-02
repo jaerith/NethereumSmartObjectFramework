481e2ecb8182abdc6bb91444957bc69438aab66f
SKIPPYNUTSHQ\Aaron <Aaron.Kendall@gmail.com>
2025-03-02 17:18:27 -0500

    Removed unnecessary property

--- NethereumSmartObjectFramework/Nethereum.Erc20.Blazor/EveFrontierModels/EveFrontierERC20ContractModel.cs
+++ NethereumSmartObjectFramework/Nethereum.Erc20.Blazor/EveFrontierModels/EveFrontierERC20ContractModel.cs
@@ -11,7 +11,5 @@ namespace Nethereum.Erc20.Blazor.EveFrontierModels
         public EveFrontierWorldModel EveFrontierWorld { get; set; } = new EveFrontierWorldModel();
 
         public EveFrontierERC20ContractModel() { }
-
-        public string ERC20Namespace { get; set; }
     }
 }
